e7f155003241a39024c07f408472fb3bfea992d2
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2020-02-18 20:21:14 +0300

    refactor(router): remove custom router

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,28 +7,28 @@ export const INITIAL_ROUTE = "WalletsOverview";
 
 const StackNavigator = createStackNavigator();
 
-const parentGetStateForAction = StackNavigator.router.getStateForAction;
-
-StackNavigator.router.getStateForAction = (action, inputState) => {
-    const state = parentGetStateForAction(action, inputState);
-
-    // fix it up if applicable
-    if (state && action.type === NavigationActions.NAVIGATE) {
-        if (action.params && action.params.replaceRoute) {
-            const leave = action.params.leave || 1;
-            delete action.params.replaceRoute;
-            while (state.routes.length > leave && state.index > 0) {
-                const oldIndex = state.index - 1;
-                // remove one that we are replacing
-                state.routes.splice(oldIndex, 1);
-                // index now one less
-                state.index = oldIndex;
-            }
-        }
-    }
-
-    return state;
-};
+// const parentGetStateForAction = StackNavigator.router.getStateForAction;
+//
+// StackNavigator.router.getStateForAction = (action, inputState) => {
+//     const state = parentGetStateForAction(action, inputState);
+//
+//     // fix it up if applicable
+//     if (state && action.type === NavigationActions.NAVIGATE) {
+//         if (action.params && action.params.replaceRoute) {
+//             const leave = action.params.leave || 1;
+//             delete action.params.replaceRoute;
+//             while (state.routes.length > leave && state.index > 0) {
+//                 const oldIndex = state.index - 1;
+//                 // remove one that we are replacing
+//                 state.routes.splice(oldIndex, 1);
+//                 // index now one less
+//                 state.index = oldIndex;
+//             }
+//         }
+//     }
+//
+//     return state;
+// };
 
 const ApplicationRouter = () => {
     return (
